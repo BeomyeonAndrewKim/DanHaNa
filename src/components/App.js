d1610b8ba29816676c0033ad0af903fae3941ded
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-06 19:34:52 +0900

    React Router 설정
    로그인 페이지 및 메인 페이지 React Router Path 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+
+import LoginScreenContainer from '../containers/LoginScreenContainer';
+import MainScreen from '../components/MainScreen';
 
 export default class extends Component {
   render() {
-    return <div>DanHaNa</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/login" component={LoginScreenContainer} />
+            <Route path="/main" component={MainScreen} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
