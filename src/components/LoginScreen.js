6ebac59b5c09ce84123339a7477b9581179b59d7
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-06 15:51:42 +0900

    LoginScreen component 생성

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <div>
+        <div>login</div>
+      </div>
+    );
+  }
+}
