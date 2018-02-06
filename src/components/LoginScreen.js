2c7862e16878cdc3c42428d043c35c462836b9c2
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-06 16:12:21 +0900

    LoginScreen Storybook 추가
    facebook으로 로그인 버튼 추가

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'antd';
 
 export default class LoginScreen extends Component {
   render() {
     return (
       <div>
-        <div>login</div>
+        <Button type="primary" ghost>
+          <Icon type="facebook" /> facebook
+        </Button>
       </div>
     );
   }
