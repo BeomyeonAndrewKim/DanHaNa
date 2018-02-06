ccc6e8c9409e3319c23e7b5656d3f21af9b19fc1
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-06 19:35:27 +0900

    MainScreen 페이지 추가

---
+++ src/components/MainScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MainScreen extends Component {
+  render() {
+    return (
+      <div>
+        <div>여기는 메인페이지!</div>
+      </div>
+    );
+  }
+}
