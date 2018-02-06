ead69247c47950922b93b514894d6918a48d7d8e
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-06 19:33:33 +0900

    LoginScreen 로그인 기능 추가
    Firebase authentication을 활용하여 페이스북 및 구글 로그인 기능 추가

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'antd';
+import styled from 'styled-components';
+
+const LoginWrap = styled.div`
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 300px;
+  margin: auto;
+`;
+const LoginList = styled.ul`
+  width: 100%;
+  list-style: none;
+`;
+const LoginListItem = styled.li``;
+const LoginButton = styled(Button)`
+  margin: auto;
+  display: inline-block;
+  width: 150px;
+`;
 
 export default class LoginScreen extends Component {
+  static defaultProp = {
+    onFacebookLogin: () => {},
+    onGoogleLogin: () => {},
+  };
   render() {
+    const { onGoogleLogin, onFacebookLogin } = this.props;
     return (
-      <div>
-        <Button type="primary" ghost>
-          <Icon type="facebook" /> facebook
-        </Button>
+      <div style={{ height: '100vh' }}>
+        <LoginWrap>
+          <LoginList>
+            <LoginListItem>
+              <LoginButton onClick={onFacebookLogin}>
+                <Icon type="facebook" />
+                Facebook
+              </LoginButton>
+            </LoginListItem>
+            <LoginListItem>
+              <LoginButton onClick={onGoogleLogin}>
+                <Icon type="google" /> Google
+              </LoginButton>
+            </LoginListItem>
+          </LoginList>
+        </LoginWrap>
       </div>
     );
   }
--- stories/LoginScreen.js
+++ stories/LoginScreen.js
@@ -3,4 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import LoginScreen from '../src/components/LoginScreen';
 
-storiesOf('LoginScreen', module).add('with text', () => <LoginScreen />);
+storiesOf('LoginScreen', module).add('default', () => (
+  <LoginScreen onGoogleLogin={action('onGoogleLogin')} />
+));
