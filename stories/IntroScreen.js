06606f6fd7cd6f741bd7d616d47c3ad50ef1de5e
BeomyeonAndrewKim <beom911@gmail.com>
2018-02-06 19:58:16 +0900

    IntroScreen Storybook 추가

---
+++ stories/IntroScreen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import IntroScreen from '../src/components/IntroScreen';
+
+storiesOf('IntroScreen', module).add('with text', () => <IntroScreen />);
