2e569ed9b377fdaa8bc75e1d20af07a318f9c8e2
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-08 18:08:37 +0900

    Storybook 폴더 경로 수정

--- stories/LoginScreen.js
+++ stories/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import LoginScreen from '../src/components/LoginScreen';
+import LoginScreen from '../src/components/Login/LoginScreen';
 
 storiesOf('LoginScreen', module).add('default', () => (
   <LoginScreen onGoogleLogin={action('onGoogleLogin')} />
--- stories/MainScreen.js
+++ stories/MainScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import MainScreen from '../src/components/MainScreen';
+import MainScreen from '../src/components/Main/MainScreen';
 
 storiesOf('MainScreen', module).add('default', () => <MainScreen />);
