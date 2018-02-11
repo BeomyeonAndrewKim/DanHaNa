64a2cceb8fb96bce0fed1a112d813ef539507d09
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-11 12:53:16 +0900

    #12 Profile컴포넌트 생성 및 CSS 적용
    CSS: 컨텐츠 가운데 정렬

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Icon, Form, Input, Upload, message } from 'antd';
+
+import ProfileHeader from './ProfileHeader';
+import ProfilePhoto from './ProfilePhoto';
+import './Profile.css';
+
+const ProfileWrap = styled.div`
+  text-align: center;
+`;
+
+const FormItem = Form.Item;
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div style={{ height: '100vh' }}>
+        <ProfileWrap>
+          <ProfileHeader />
+          <ProfilePhoto />
+          <div>
+            <Form layout="inline">
+              <FormItem label="Username">
+                <Input />
+              </FormItem>
+            </Form>
+          </div>
+        </ProfileWrap>
+      </div>
+    );
+  }
+}
