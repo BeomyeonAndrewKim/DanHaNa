821b485d1b2ca7d290addd36721c229a29da3f4c
BeomyeonAndrewKim <beom911@gmail.com>
2018-02-11 19:27:47 +0900

    #13 MenuProfile 컴포넌트 내 이미지 및 닉네임 데이터 경로 변경
    테스트용으로 설정해 놓은 닉네임과 이미지 경로를
    MenuProfileContainer에서 Props값으로 가져온 Firebase Database 값으로 변경

--- src/components/Menu/MenuProfile.js
+++ src/components/Menu/MenuProfile.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 export default class MenuProfile extends Component {
+  static defaultProps = {
+    nickName: '',
+    photoUrl: '',
+  };
   render() {
     return (
       <div>
@@ -8,9 +12,9 @@ export default class MenuProfile extends Component {
           <img
             className="profileImg"
             alt="profilimg"
-            src="https://firebasestorage.googleapis.com/v0/b/danhana-dev.appspot.com/o/1492752277484.jpg?alt=media&token=4fdb2365-3118-4dcd-bf67-d0dbeace4e93"
+            src={this.props.photoUrl}
           />
-          <figcaption>BeomyeonKim</figcaption>
+          <figcaption>{this.props.nickName}</figcaption>
         </figure>
       </div>
     );
