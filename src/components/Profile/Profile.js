1f595df4f99516bdf2ad602a02b6400c4ff6917c
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-11 21:50:53 +0900

    #12 사용하지 않는 태그 삭제

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Icon, Form, Input, Upload, message } from 'antd';
+import { Form, Input } from 'antd';
 
 import ProfileHeader from './ProfileHeader';
 import ProfilePhoto from './ProfilePhoto';
