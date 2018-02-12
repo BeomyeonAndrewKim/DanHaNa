2844a35297b807fd8f3ee3b2b24ee5fda7209340
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-12 22:22:41 +0900

    #12 컴포넌트 세분화
    1. ProfileHeader: 상단 헤더바

--- src/components/Profile/ProfileScreen.js
+++ src/components/Profile/ProfileScreen.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Form, Input } from 'antd';
 
 import ProfileHeader from './ProfileHeader';
-import ProfilePhoto from './ProfilePhoto';
-import './Profile.css';
+import ProfilePhotoContainer from '../../containers/Profile/ProfilePhotoContainer';
+import ProfileNickNameContainer from '../../containers/Profile/ProfileNickNameContainer';
+
+import './ProfileScreen.css';
 
 const ProfileWrap = styled.div`
   text-align: center;
+  height: 100vh;
 `;
-
-const FormItem = Form.Item;
 
 export default class ProfileScreen extends Component {
   render() {
     return (
-      <div style={{ height: '100vh' }}>
-        <ProfileWrap>
-          <ProfileHeader />
-          <ProfilePhoto />
-          <div>
-            <Form layout="inline">
-              <FormItem label="Username">
-                <Input />
-              </FormItem>
-            </Form>
-          </div>
-        </ProfileWrap>
-      </div>
+      <ProfileWrap>
+        <ProfileHeader />
+        <ProfilePhotoContainer />
+        <ProfileNickNameContainer />
+      </ProfileWrap>
     );
   }
 }
