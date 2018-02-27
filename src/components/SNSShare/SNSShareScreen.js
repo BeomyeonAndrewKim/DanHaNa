dc03b996a32f5a96bab546ecf71822ff775e8da7
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-27 17:24:20 +0900

    test code

--- src/components/SNSShare/SNSShareScreen.js
+++ src/components/SNSShare/SNSShareScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
 
 export default class SNSShareScreen extends Component {
   componentWillMount() {
@@ -23,26 +22,10 @@ export default class SNSShareScreen extends Component {
   render() {
     return (
       <div>
-        <Helmet>
-          <meta
-            property="og:url"
-            content="https://Danhana.netlify.com.netlify.com/snsshare"
-          />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content="단 하나" />
-          <meta
-            property="og:description"
-            content="일주일에 단 하나의 목표를 설정하고 완료해보세요"
-          />
-          <meta
-            property="og:image"
-            content="https://firebasestorage.googleapis.com/v0/b/test-368e1.appspot.com/o/users%2FKerCHi8BRYeESR01zicIEPVtMDv1%2FphotoURL?alt=media&token=49c47130-a7ac-4e02-b7d8-f4df585fb8ce"
-          />
-        </Helmet>
         <div id="fb-root">dd</div>
         <div
           className="fb-share-button"
-          data-href="https://Danhana.netlify.com.netlify.com/snsshare"
+          data-href="https://developers.facebook.com/docs/plugins/"
           data-layout="button"
           data-size="small"
           data-mobile-iframe="true"
