cae685fccdcb5b4f2e1ab45d8f725f96939dff6b
BeomyeonAndrewKim <beom911@gmail.com>
2018-03-05 00:56:54 +0900

    #48 todoList fetch함수 재사용 위한 calendar.js redux추가

---
+++ src/ducks/calendar.js
@@ -0,0 +1,54 @@
+import * as firebase from 'firebase';
+
+export const GET_TODOLIST_REQUEST = 'profile/GET_TODOLIST_REQUEST';
+export const GET_TODOLIST_SUCCESS = 'profile/GET_TODOLIST_SUCCESS';
+
+export function getTodoListRequest() {
+  return {
+    type: GET_TODOLIST_REQUEST,
+  };
+}
+export function getTodoListSuccess(todoList) {
+  return {
+    type: GET_TODOLIST_SUCCESS,
+    todoList,
+  };
+}
+
+const initialState = {
+  loading: false,
+  todoList: [],
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_TODOLIST_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_TODOLIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        todoList: action.todoList,
+      };
+    default:
+      return state;
+  }
+}
+
+export const fetchTodoList = () => async dispath => {
+  const { currentUser } = firebase.auth();
+  if (currentUser) {
+    const { uid } = currentUser;
+    dispath(getTodoListRequest());
+    const snapshot = await firebase
+      .database()
+      .ref(`users/${uid}/todos`)
+      .once('value');
+    const todoListObj = snapshot.val();
+    const todoList = Object.entries(todoListObj);
+    dispath(getTodoListSuccess(todoList));
+  }
+};
--- src/ducks/index.js
+++ src/ducks/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import main from './main';
 import profile from './profile';
 import mission from './mission';
+import calendar from './calendar';
 
 const reducers = combineReducers({
   main,
   profile,
   mission,
+  calendar,
 });
 
 export default reducers;
