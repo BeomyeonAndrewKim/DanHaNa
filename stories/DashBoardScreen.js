733560299575ee5dceb0d1c6c80d1f43271bb494
BeomyeonAndrewKim <beom911@gmail.com>
2018-03-05 00:57:12 +0900

    #48 통계 컴포넌트 storybook추가

---
+++ stories/DashBoardScreen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import DashboardScreen from '../src/components/Dashboard/DashboardScreen';
+
+storiesOf('Dashboard', module).add('default', () => <DashboardScreen />);
