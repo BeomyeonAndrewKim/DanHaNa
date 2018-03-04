29bebc631a1509281d36f5ed4f1df3bead1def11
Jang Jaehoon <idlinlee12@gmail.com>
2018-03-04 15:18:58 +0900

    #29 파이어베이스 데이터베이스 todos 하위 목록 가져오는 덕스 생성

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import main from './main';
 import profile from './profile';
+import calendar from './calendar';
 
 const reducers = combineReducers({
   main,
   profile,
+  calendar,
 });
 
 export default reducers;
