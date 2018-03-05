79c6a945c525ff5c76c6dafd4b2585f362695556
Jang Jaehoon <idlinlee12@gmail.com>
2018-03-05 14:27:52 +0900

    #34 SNS 공유 기능에서 meta 태그를 인식하지 못하던 오류 해결

--- src/containers/Login/LoginScreenContainer.js
+++ src/containers/Login/LoginScreenContainer.js
@@ -82,6 +82,7 @@ export default class LoginScreenContainer extends Component {
     }
   };
   render() {
+    window.prerenderReady = true;
     if (window.localStorage.getItem('introdone') || this.state.redirectToMain) {
       return <Redirect to="/main" />;
     }
