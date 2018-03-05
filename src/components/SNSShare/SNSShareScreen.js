91f3c07599507e6a33998325ef4ff7218293dd13
Jang Jaehoon <idlinlee12@gmail.com>
2018-03-05 14:28:19 +0900

    #34 SNS 공유버튼과 스크린 컴포넌트 구분

--- src/components/SNSShare/SNSShareScreen.js
+++ src/components/SNSShare/SNSShareScreen.js
@@ -1,46 +1,10 @@
 import React, { Component } from 'react';
 
 export default class SNSShareScreen extends Component {
-  componentWillMount() {
-    (function(d, s, id) {
-      let js = null,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src =
-        'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&autoLogAppEvents=1&version=v2.12&appId=292444687949351';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  }
-  share = () => {
-    window.open(
-      'http://www.facebook.com/sharer/sharer.php?u=http://eel7891.com',
-    );
-  };
-
   render() {
     return (
       <div>
-        <div id="fb-root">dd</div>
-        <div
-          className="fb-share-button"
-          data-href="https://developers.facebook.com/docs/plugins/"
-          data-layout="button"
-          data-size="small"
-          data-mobile-iframe="true"
-        >
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-            className="fb-xfbml-parse-ignore"
-          >
-            공유하기
-          </a>
-        </div>
-        <div onClick={this.share}>asdfaiosjdfoijasdof</div>
-        <div />
+        <div>dd</div>
       </div>
     );
   }
