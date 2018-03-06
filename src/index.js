b175cd4d14cf7e58e25677e9de17cb2fba167e02
BeomyeonAndrewKim <beom911@gmail.com>
2018-03-07 03:03:45 +0900

    #54 불필요한 less파일 제거

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import * as firebase from 'firebase';
 
 import 'animate.css';
-import './less/src/less/index.css';
+import './less/index.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
