4814268f2c8dbe690c5bc8cee9e704b61c964f1c
Jang Jaehoon <idlinlee12@gmail.com>
2018-03-07 10:52:43 +0900

    #51 ProfilePhoto 컴포넌트 스타일 네이밍 변경 및 공통 CSS 제거

--- src/components/Profile/ProfileScreen.js
+++ src/components/Profile/ProfileScreen.js
@@ -11,28 +11,6 @@ import './ProfileScreen.css';
 const ProfileWrap = styled.div`
   text-align: center;
   height: 100vh;
-  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#dc7036+50,c45b2d+50 */
-  background: #dc7036; /* Old browsers */
-  background: -moz-linear-gradient(
-    -45deg,
-    #dc7036 50%,
-    #c45b2d 50%
-  ); /* FF3.6-15 */
-  background: -webkit-linear-gradient(
-    -45deg,
-    #dc7036 50%,
-    #c45b2d 50%
-  ); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(
-    135deg,
-    #dc7036 50%,
-    #c45b2d 50%
-  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient(
-      startColorstr='#dc7036',
-      endColorstr='#c45b2d',
-      GradientType=1
-    ); /* IE6-9 fallback on horizontal gradient */
 `;
 const ProfileTextInfo = styled.div`
   margin: 0 15px;
