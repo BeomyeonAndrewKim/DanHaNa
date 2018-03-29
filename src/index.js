52160685e32873a0c245db6efbcf3d5f8ab4ea36
BeomyeonAndrewKim <beom911@gmail.com>
2018-03-29 23:39:02 +0900

    custom-theme 폴더명 변경

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as firebase from 'firebase';
 import 'animate.css';
-import './less/custom-theme.css';
+import './theme/custom-theme.css';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
