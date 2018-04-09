a067f5c81268b7eecc7383fe4bc1c2f4afca031c
BeomyeonAndrewKim <beom911@gmail.com>
2018-04-09 23:46:07 +0900

    #34 snsshare컴포넌트의 데이터 통신 부분 수정
    match.params 프로퍼티를 활용해야 하기 때문에
    redux대신 componentWillMount내에서 firebase 데이터 가져오기

--- src/containers/Main/SNSMainScreenContainer.js
+++ src/containers/Main/SNSMainScreenContainer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import * as moment from 'moment';
+import * as firebase from 'firebase';
 import SNSMainScreen from '../../components/Main/SNSMainScreen';
 import withLoadingIndicator from '../../hocs/withLoadingIndicator';
-import { fetchBothInfo, fetchTodoInfo } from '../../ducks/main';
 
 const SNSMainScreenWithLoading = withLoadingIndicator(SNSMainScreen);
 
-class SNSMainScreenContainer extends Component {
+const THIS_WEEK = moment()
+  .isoWeekday(1)
+  .format('YYYY-[W]ww');
+export default class SNSMainScreenContainer extends Component {
   static defaultProps = {
     loading: false,
     userInfo: {},
@@ -14,33 +17,32 @@ class SNSMainScreenContainer extends Component {
     onMount: () => {},
   };
 
-  componentWillMount() {
-    this.props.onMount();
+  state = {
+    userInfo: {},
+    todoInfo: {},
+  };
+  async componentWillMount() {
+    const snapshot = await firebase
+      .database()
+      .ref(`users/${this.props.match.params.uid}`)
+      .once('value');
+    const userObj = snapshot.val();
+    const userInfo = userObj.profileInfo;
+    const todoInfo = userObj.todos[THIS_WEEK];
+    this.setState({
+      userInfo,
+      todoInfo,
+    });
   }
   render() {
     return (
       <div>
         <SNSMainScreenWithLoading
-          lodaing={this.props.loading}
-          userInfo={this.props.userInfo}
-          todoInfo={this.props.todoInfo}
+          loading={!this.state.userInfo.uid}
+          userInfo={this.state.userInfo}
+          todoInfo={this.state.todoInfo}
         />
       </div>
     );
   }
 }
-export default connect(
-  state => ({
-    loading: state.main.loading,
-    todoInfo: state.main.todoInfo,
-    userInfo: state.main.userInfo,
-  }),
-  dispatch => ({
-    onMount: () => {
-      dispatch(fetchBothInfo());
-    },
-    onLoadTodo: () => {
-      dispatch(fetchTodoInfo());
-    },
-  }),
-)(SNSMainScreenContainer);
